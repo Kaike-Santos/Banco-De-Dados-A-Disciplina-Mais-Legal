5f3d2908bd40d27f5e10ea9b7205c0acd2fc03d1
kassike <kaikems27@outlook.com>
2023-09-26 23:16:38 -0300

    2o exercicio

--- 2 Exercicios.sql
+++ 2 Exercicios.sql
@@ -8,3 +8,18 @@ END;
 //
 DELIMITER ;
 call sp_ListarAutores();
+
+-- Desenvolva uma stored procedure chamada sp_LivrosPorCategoria que aceite o nome de uma categoria como parâmetro e
+--  retorne todos os livros que pertencem a essa categoria. Não se esqueça de testar a procedure com diferentes categorias.
+DELIMITER //
+create procedure sp_LivrosPorCategoria (IN nomeCategoria Varchar(50)  ) 
+BEGIN
+SELECT livro.titulo  
+from livro
+INNER JOIN categoria ON livro.Categoria_ID = categoria.Categoria_ID
+where categoria.nome = nomeCategoria
+; 
+END;
+//
+DELIMITER ;
+call sp_LivrosPorCategoria ('romance');
